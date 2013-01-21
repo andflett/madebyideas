724039b80af253832c3d6db501e4fae557e300f0
Andrew Sprinz <andrewsprinz@gmail.com>
2013-01-21 23:10:09 +0000

    h

--- db/migrate/20130121225949_casting.rb
+++ db/migrate/20130121225949_casting.rb
@@ -3,22 +3,22 @@ class Casting < ActiveRecord::Migration
 		connection.execute(%q{
 		    alter table ratings
 		    alter column user_id
-		    type integer using cast(number as integer)
+		    type integer using cast(user_id as integer)
 		})
 		connection.execute(%q{
 		    alter table ratings
 		    alter column post_id
-		    type integer using cast(number as integer)
+		    type integer using cast(post_id as integer)
 		})
 		connection.execute(%q{
 		    alter table notifications
 		    alter column user_id
-		    type integer using cast(number as integer)
+		    type integer using cast(user_id as integer)
 		})
 		connection.execute(%q{
 		    alter table notifications
 		    alter column post_id
-		    type integer using cast(number as integer)
+		    type integer using cast(post_id as integer)
 		})
   end
 
